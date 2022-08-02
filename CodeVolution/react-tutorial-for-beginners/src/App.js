8e26393162e059b805c1a71d4af23ba7b9be833e
Kamdev Kumar <kamdevkumar666@gmail.com>
2022-08-02 22:57:09 +0530

    code added

--- CodeVolution/react-tutorial-for-beginners/src/App.js
+++ CodeVolution/react-tutorial-for-beginners/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ParentComp from "./component/1.Pure Components/ParentComp";
+import PostList from "./component/10.HTTP GET/PostList";
+import ParentinMemo from "./component/2.Memo/ParentinMemo";
+import RefsDemo from "./component/3.Refs/RefsDemo";
+import ForwardRefParentInput from "./component/4.Forwarding Refs/ForwardRefParentInput";
+import PortalDemo from "./component/5.Portals/PortalDemo";
+import ErrCompParent from "./component/6.ErrorBoundary/ErrCompParent";
+import ClickCounter from "./component/7.HOC/ClickCounter";
+import HoverCounter from "./component/7.HOC/HoverCounter";
+import Starting from "./component/9.Context/Starting";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<ParentComp/>*/}
+      {/*<ParentinMemo/>*/}
+      {/*<RefsDemo/>*/}
+      {/*<ForwardRefParentInput/> */}
+      {/*<PortalDemo/>*/}
+      {/*<ErrCompParent/>*/}
+      {/*<ClickCounter/>
+       <HoverCounter/> */}
+      {/*<Starting />*/}
+      <PostList/>
     </div>
   );
 }
