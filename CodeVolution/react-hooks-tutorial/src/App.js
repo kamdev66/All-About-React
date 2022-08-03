f6edfb36ecd389495dbffb31147520d3998f8c32
Kamdev Kumar <kamdevkumar666@gmail.com>
2022-08-03 16:44:54 +0530

    more code added

--- CodeVolution/react-hooks-tutorial/src/App.js
+++ CodeVolution/react-hooks-tutorial/src/App.js
@@ -1,9 +1,18 @@
+import HookCounter from './All Hooks/1.UseState Hook/HookCounter';
+import HookCounterFour from './All Hooks/1.UseState Hook/useStatewithArray/HookCounterFour';
+import HookCounterThree from './All Hooks/1.UseState Hook/useStatewithObject/HookCounterThree';
+import HookCounterTwo from './All Hooks/1.UseState Hook/useStatewithPrevstate/HookCounterTwo';
+import EffectCounterOne from './All Hooks/2.UseEffect/EffectCounterOne';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>kkkkk</h1>
+      {/*<HookCounter/>*/}
+      {/*<HookCounterTwo/>*/}
+      {/*<HookCounterThree/>*/}
+      {/*<HookCounterFour/>*/}
+      <EffectCounterOne/>
     </div>
   );
 }
